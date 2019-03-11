3454397636087a1c7eb739292824699f7311f939
Lavesh10 <48425923+Lavesh10@users.noreply.github.com>
2019-03-11 14:33:19 +0530

    first file

--- calci.java
+++ calci.java
@@ -1,3 +1,14 @@
-java
-basic java
-advance java
+import java.io.*;
+import java.util.*;
+class calci
+{
+  public static void main(String args[])throws IOException
+  {
+    Scanner in=new Scanner(System.in);
+    System.out.println("Enter two no.s");
+    int no1=in.nextInt();
+    int no2=in.nextInt();
+    int sum=no1+no2;
+    System.out.println("sum="+sum);
+  }
+}
